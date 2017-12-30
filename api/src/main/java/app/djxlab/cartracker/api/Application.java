bac2a652aa14f8d90985ab2b9fa1d1baed6e7cb3
dhavaljoshi008 <dhavaljoshi008@gmail.com>
2017-12-30 13:03:26 -0600

    chore(structure): add api folder wih supporting files

---
+++ api/src/main/java/app/djxlab/cartracker/api/Application.java
@@ -0,0 +1,17 @@
+/**
+ * Application.java
+ * app.djxlab.cartracker.api 
+ *
+ * @author: Dhaval Joshi.
+ */
+package app.djxlab.cartracker.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+    		SpringApplication.run(Application.class, args);
+    }
+}
