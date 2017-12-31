80c4227ed7eef291f7b648e594573a8a79c6bdc2
dhavaljoshi008 <dhavaljoshi008@gmail.com>
2017-12-30 23:25:48 -0600

    feat(module-api): add cross-origin support

--- api/src/main/java/app/djxlab/cartracker/api/controller/VehicleController.java
+++ api/src/main/java/app/djxlab/cartracker/api/controller/VehicleController.java
@@ -9,6 +9,7 @@ package app.djxlab.cartracker.api.controller;
 import java.util.List;
 
 import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -26,11 +27,13 @@ public class VehicleController {
 		this.vehicleService = vehicleService;
 	}
 	
+    @CrossOrigin(origins = "http://mocker.egen.io")
 	@RequestMapping(value = "/vehicle", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public Vehicle putVehicle(@RequestBody Vehicle vehicle) {
 		return vehicleService.save(vehicle);
 	}
 	
+    @CrossOrigin(origins = "http://mocker.egen.io")
 	@RequestMapping(value = "/vehicles", method = RequestMethod.PUT, consumes = MediaType.APPLICATION_JSON_UTF8_VALUE, produces = MediaType.APPLICATION_JSON_UTF8_VALUE)
 	public List<Vehicle> putVehicles(@RequestBody List<Vehicle> vehicles) {
 		return vehicleService.save(vehicles);
